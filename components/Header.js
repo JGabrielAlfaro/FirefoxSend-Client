4bb24c8064a279d40f2ac3fab7a0637d66eed338
Gabriel Alfaro <gabriel.alfaro.cruz@gmail.com>
2023-12-21 13:52:33 -0600

    Login y Crear cuenta - Validacion Formik y yup

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <header className='py-8 flex flex-col md:flex-row items-center justify-between'>
-      <img src='logo.svg' className='w-64 mb-8 md:mb-0'/>
+      <Link href={"/"}>
+          <img src='logo.svg' className='w-64 mb-8 md:mb-0'/>
+      </Link>
+      
+      <div>
+          <Link 
+            href={"/login"} 
+            className='bg-red-500 px-5 py-3 rounded-lg text-white font-bold uppercase mr-2'
+          > Iniciar Sesion</Link>
+
+          <Link 
+            href={"/crearcuenta"} 
+            className='bg-black px-5 py-3 rounded-lg text-white font-bold uppercase'
+          > Crear Cuenta</Link>
+      </div>
     </header>
   )
 }
--- pages/crearcuenta.js
+++ pages/crearcuenta.js
@@ -1,28 +1,30 @@
+
 import Layout from "@/components/Layout"
 import { useFormik } from "formik"
 import * as Yup from 'yup'
 
-export default function Login() {
+export default function CrearCuenta() {
 
-   //Formulario y validacion con formik
-   const formik = useFormik({
-    initialValues: {
-        email: '',
-        password: ''
-    },
-    validationSchema: Yup.object({
-        email: Yup.string().email("El email no es valido").required('El email es Obligatorio'),
-        password:  Yup.string().required('El password es Obligatorio')
-    }),
-    onSubmit: (data) =>{
-        console.log("Enviando formulario", data)
-    }
-})
-
+    //Formulario y validacion con formik
+    const formik = useFormik({
+        initialValues: {
+            nombre: '',
+            email: '',
+            password: ''
+        },
+        validationSchema: Yup.object({
+            nombre: Yup.string().required('El nombre es Obligatorio'),
+            email: Yup.string().email("El email no es valido").required('El email es Obligatorio'),
+            password:  Yup.string().required('El password es Obligatorio').min(6,"El password debe contener al menos 6 caracteres")
+        }),
+        onSubmit: (data) =>{
+            console.log("Enviando formulario", data)
+        }
+    })
   return (
     <Layout>
       <div className="md:w-4/5 xl:w-3/5 mb-32">
-        <h2 className="text-4xl font-sans font-bold text-center text-gray-800 my-4">Iniciar Sesión</h2>
+        <h2 className="text-4xl font-sans font-bold text-center text-gray-800 my-4">Crear cuenta</h2>
 
         <div className="flex justify-center mt-5">
             <div className="w-full max-w-lg">
@@ -31,6 +33,29 @@ export default function Login() {
                     onSubmit={formik.handleSubmit}
                 >
                     <div className="mb-4">
+
+                        {/* NOMBRE */}
+                        <label 
+                            className="block text-black text-sm font-bold mb-2"
+                            htmlFor="nombre"
+                        >Nombre</label>
+                        <input
+                            type="text"
+                            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shawow-outline"
+                            id="nombre"
+                            placeholder="Nombre de Usuario"
+                            value={formik.values.nombre}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
+                        />
+
+                        {formik.touched.nombre && formik.errors.nombre ? (
+                            <div className="my-2 bg-gray-200 border-l-4 border-red-500 text-red-700 p-4"> 
+                                <p className="font-bold">Error</p>
+                                <p >{formik.errors.nombre}</p>
+                            </div>
+                        ) : null }
+
 
                          {/* EMAIL */}
                         <label 
@@ -78,7 +103,7 @@ export default function Login() {
                     <input 
                         type="submit"
                         className="bg-red-500 hover:bg-gray-500 w-full p-2 text-white uppercase font-bold"
-                        value={"Iniciar Sesión"}
+                        value={"Crear cuenta"}
                     />
                 </form>
 
